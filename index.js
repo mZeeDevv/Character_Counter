079958ad5cded3880ea7b0ca9149570bdc389f47
Hamza Shaukat <hamzeekhattak56@gmail.com>
2023-02-14 22:38:12 +0500

    no-drop options for Cursor

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ let btn = document.getElementById('btn');
 let textarea = document.getElementById('text');
 let value = document.getElementById('value');
 let para = document.getElementById('para');
-
 textarea.onkeyup = function(){
      value.innerHTML = textarea.value.length;
-
      if(textarea.value.length == 50){
         para.style.color = "red"
+       btn.style.cursor = "no-drop"
      } else {
         para.style.color = "black"
+     btn.style.cursor = "pointer"
      }
 }
